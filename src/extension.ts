caf01b197b441af1414a0a9f9fde6c9cb4d66a12
qinghua.wang <qinghua.wang@oneildata.com>
2024-03-05 13:55:05 -0600

    Add Repeated support

--- src/extension.ts
+++ src/extension.ts
@@ -1,44 +1,57 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
 
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "flyway-migration-helper" is now active!');
+	console.log('Congratulations, your extension "flyway-smith" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('flyway-migration-helper.createMigration', async (folder: vscode.Uri) => {
-		const name = await vscode.window.showInputBox({ prompt: 'Enter the name of the migration' });
-
+	let disposable_V = vscode.commands.registerCommand('flyway-migration-helper.createVersionedMigration', async (folder: vscode.Uri) => {
+		const name = await vscode.window.showInputBox({ prompt: 'Enter the name of the versioned migration' });
 		if (name) {
-			const sanitized_name = name.replace(/\s+/g, ' ').trim().replace(/ /g, '_').toLowerCase();
-			const fileName = 'V' + formatDate(new Date()) + '__' + sanitized_name + '.sql';
-			const uri = vscode.Uri.file(path.join(folder.fsPath, fileName));
-			await vscode.workspace.fs.writeFile(uri, new Uint8Array());
+			const sanitized_name = getSanitizedName(name);
+			const fileName = `V${formatDate(new Date())}__${sanitized_name}.sql`;
+			await crateFile(folder.fsPath, fileName);
+		}
+	});
+	context.subscriptions.push(disposable_V);
 
-			vscode.window.showInformationMessage(`File ${fileName} created.`);
+	let disposable_R = vscode.commands.registerCommand('flyway-migration-helper.createRepeatedMigration', async (folder: vscode.Uri) => {
+		const name = await vscode.window.showInputBox({ prompt: 'Enter the name of the repeated migration' });
+		if (name) {
+			const sanitized_name = getSanitizedName(name);
+			const fileName = `R__${sanitized_name}.sql`;
+			await crateFile(folder.fsPath, fileName);
 		}
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable_R);
+}
 
-	context.subscriptions.push(disposable);
+async function crateFile(dest: string, fileName: string) {
+	const uri = vscode.Uri.file(path.join(dest, fileName));
+	await vscode.workspace.fs.writeFile(uri, new Uint8Array());
+
+	vscode.window.showInformationMessage(`File ${fileName} created.`);
+}
+
+function getSanitizedName(name: string): string {
+	return name.replace(/\s+/g, ' ').trim().replace(/ /g, '_').toLowerCase();
 }
 
 function formatDate(date: Date): string {
 	const year = date.getFullYear();
-	const month = (date.getMonth() + 1).toString().padStart(2, '0');
-	const day = date.getDate().toString().padStart(2, '0');
-	const hours = date.getHours().toString().padStart(2, '0');
-	const minutes = date.getMinutes().toString().padStart(2, '0');
-	const seconds = date.getSeconds().toString().padStart(2, '0');
+	const month = (date.getUTCMonth() + 1).toString().padStart(2, '0');
+	const day = date.getUTCDate().toString().padStart(2, '0');
+	const hours = date.getUTCHours().toString().padStart(2, '0');
+	const minutes = date.getUTCMinutes().toString().padStart(2, '0');
+	const seconds = date.getUTCSeconds().toString().padStart(2, '0');
 
-	return `${year}${month}${day}${hours}${minutes}${seconds}`;
+	return `${year}.${month}.${day}.${hours}.${minutes}.${seconds}`;
 }
 
 // This method is called when your extension is deactivated
